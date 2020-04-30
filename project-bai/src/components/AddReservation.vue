03790902302b044d6634f38eb3f89f93db06a098
Dominik Woźniczka <dominikwozniczka96@gmail.com>
2020-04-30 21:09:00 +0200

    style for addreservation added

--- project-bai/src/components/AddReservation.vue
+++ project-bai/src/components/AddReservation.vue
@@ -1,22 +1,29 @@
 <template>
     <div>
         <h3>Add a reservation</h3>
+        <div v-if="isConnected">
         <div class="add"> 
             <form @submit="onSubmit" action="https://vuejs.org/" method="post">  
-                <span><b>First Name:</b></span>
+                <span ><b>First Name:</b></span>
                 <input type="text" v-model="reservation.name">
-                <span><b>Store:</b></span>
+                <span><b>Pickup:</b></span>
                 <select v-model="reservation.store">
                     <option disabled value="">Please choose:</option>
-                    <option>Store number 1</option>
-                    <option>Store number 2</option>
+                    <option>Store Karmelicka</option>
+                    <option>Store k.Gurgacza</option>
                 </select>
-                <span><b>Number of bicycles:</b></span>
+                <span><b>Number of segways:</b></span>
                 <input type="number" v-model="reservation.amount" min="1">
                 <span><b>Comment:</b></span>
                 <input type="text" v-model="reservation.comment" placeholder="Add a comment...">
                 <input type="submit" value="Submit">
             </form>
+        </div>
+        </div>
+        <div v-else>
+                    <p class="lead">Please log in by faceook if you want to make reservation.</p>
+
+
         </div>
     </div>
 </template>
@@ -26,6 +33,10 @@ import {mapActions} from 'vuex';
 
 export default {
     name: "AddReservation",
+    props:{
+        userName: String,
+        isConnected:Boolean
+    },
     data() {
         return {
             reservation: {
@@ -36,7 +47,18 @@ export default {
             }
         }
     },
+    watch:{
+        'userName': function (){
+            this.setName(this.userName)
+        },
+        'isConnected': function (){
+            this.setName(this.userName)
+        }
+    },
     methods: {
+        setName(userName){
+            this.reservation.name = userName
+        },
         ...mapActions(['addReservation']),
         onSubmit(e){
             e.preventDefault();
@@ -63,6 +85,10 @@ input[type="submit"] {
     color: #fff;
     border: 1px #41b883 solid;
     cursor: pointer;
+}
+.h3{
+    text-decoration-color: red;
+    color: red;
 }
 
 </style>
